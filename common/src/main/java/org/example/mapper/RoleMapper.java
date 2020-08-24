38d7f1cdb095469931af2c778f0dba0e108cbdbf
林兴 <2095385504@qq.com>
2020-08-24 12:01:50 +0800

    用户模块

--- common/src/main/java/org/example/mapper/RoleMapper.java
+++ common/src/main/java/org/example/mapper/RoleMapper.java
@@ -1,10 +1,19 @@
 package org.example.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.example.pojo.Role;
 
 import java.util.List;
 @Mapper
 public interface RoleMapper {
-    public List<Role> showRole();
+    public List<Role> showRole(@Param("roleName")String roleName,@Param("remarks")String remarks);  //条件查询角色
+
+    public Role getRoleId(@Param("id")int id);  //根据ID查询角色信息
+
+    public int updateRole(@Param("roleName")String roleName,@Param("remarks")String remarks,@Param("id")int id); //修改角色信息
+
+    public int addRole(@Param("roleName")String roleName,@Param("remarks")String remarks);
+
+    public int deleteRole(@Param("id")int id);
 }
